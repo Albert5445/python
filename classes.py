6d61741c37b4639bc58717367feae01ba13a39f5
Albert <albert.khachatryan.goya@gmail.com>
2023-09-28 18:34:41 +0400

    My Rectengle

--- classes.py
+++ classes.py
@@ -1,12 +1,50 @@
-class Goya:
-    def say_about_you(self):
-        print("My name is " + self.name)
+class Rectengle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        return f"This is a Rectengle with weight {self.width} , height {self.height}"
 
 
-r1 = Goya
-r1.name = "Albert"
-r1.age = 21
-r1.profetion = "programer"
+    def __repr__(self):
+        return "This is a Rectengle code "
+
+    def __eq__(self, other):
+        if isinstance(other , Rectengle):
+           return (self.width, self.height) == (other.width, other.height)
+        else:
+            return False
+
+    def __lt__(self, other):
+        if isinstance(other, Rectengle):
+            return self.area() < other.area()
+        else:
+            return NotImplemented
+    def __le__(self, other):
+        if isinstance(other, Rectengle):
+            return self.area() <= other.area()
+        else:
+            return NotImplemented
+    def __gt__(self, other):
+        if isinstance(other, Rectengle):
+            return self.area() > other.area()
+        else:
+            return NotImplemented
+    def __ge__(self, other):
+        if isinstance(other , Rectengle):
+            return self.area() >= other.area()
+        else:
+            return NotImplemented
 
 
-r1.say_about_you()
+r1 = Rectengle(10, 20)
+r2 = Rectengle(100, 200)
+
+print(r1 < r2)
